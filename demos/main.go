b31e6894fd4c389d8a9181126fe96d0b614a4d63
James Dunne <james.jdunne@gmail.com>
2014-07-15 11:23:58 -0400

    Starting to refactor demos/main.go to use go-util/base package.

--- demos/main.go
+++ demos/main.go
@@ -22,6 +22,9 @@ import (
 	"syscall"
 )
 
+import "github.com/JamesDunne/go-util/base"
+import "github.com/JamesDunne/go-util/fs/notify"
+
 var proxyRoot, jailRoot, accelRedirect string
 var jplayerUrl, jplayerPath string
 var useJPlayer bool
@@ -736,46 +739,33 @@ func main() {
 	var socketType string
 	var socketAddr string
 
-	// TODO(jsd): Make this pair of arguments a little more elegant, like "unix:/path/to/socket" or "tcp://:8080"
-	flag.StringVar(&socketType, "l", "tcp", `type of socket to listen on; "unix" or "tcp" (default)`)
-	flag.StringVar(&socketAddr, "a", ":8080", `address to listen on; ":8080" (default TCP port) or "/path/to/unix/socket"`)
+	fl_listen_uri := flag.String("l", "tcp://0.0.0.0:8080", "listen URI (schemes available: tcp, unix)")
 	flag.StringVar(&proxyRoot, "p", "/", "root of web requests to process")
 	flag.StringVar(&jailRoot, "r", ".", "local filesystem path to bind to web request root path")
+	htmlPath := flag.String("html", "./html", "local filesystem path to HTML templates")
 	flag.StringVar(&accelRedirect, "xa", "", "Root of X-Accel-Redirect paths to use)")
-	flag.StringVar(&jplayerUrl, "jp-url", "", `Web path to jPlayer files (e.g. "/js")`)
-	flag.StringVar(&jplayerPath, "jp-path", "", `Local filesystem path to jPlayer files`)
+	flag.StringVar(&jplayerUrl, "jp-url", "", `Web path to jPlayer files (e.g. "/js" or "//static.somesite.com/jp")`)
+	flag.StringVar(&jplayerPath, "jp-path", "", `Local filesystem path to serve jPlayer files from (optional)`)
 	flag.Parse()
+
+	// Parse all the URIs:
+	listen_addr, err := base.ParseListenable(*fl_listen_uri)
+	base.PanicIf(err)
 
 	if jplayerUrl != "" {
 		useJPlayer = true
 	}
 
-	// Create the socket to listen on:
-	l, err := net.Listen(socketType, socketAddr)
+	// Watch the html templates for changes and reload them:
+	_, err, cleanup := watchTemplates("ui", htmlPath, "*.html")
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	defer cleanup()
 
-	// NOTE(jsd): Unix sockets must be unlink()ed before being reused again.
-
-	// Handle common process-killing signals so we can gracefully shut down:
-	sigc := make(chan os.Signal, 1)
-	signal.Notify(sigc, os.Interrupt, os.Kill, syscall.SIGTERM)
-	go func(c chan os.Signal) {
-		// Wait for a signal:
-		sig := <-c
-		log.Printf("Caught signal '%s': shutting down.", sig)
-		// Stop listening:
-		l.Close()
-		// Delete the unix socket, if applicable:
-		if socketType == "unix" {
-			os.Remove(socketAddr)
-		}
-		// And we're done:
-		os.Exit(0)
-	}(sigc)
-
-	// Start the HTTP server:
-	log.Fatal(http.Serve(l, http.HandlerFunc(processRequest)))
+	// Start the server:
+	base.ServeMain(listen_addr, func(l net.Listener) error {
+		return http.Serve(l, http.HandlerFunc(processRequest))
+	})
 }
